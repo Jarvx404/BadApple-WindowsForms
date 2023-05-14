765de33b72c86f66ce7821e2fa89ff028beee066
Jarvx404 <jarvx404@gmail.com>
2023-05-14 21:59:10 +0300

    Inital commit

---
+++ BadAppleForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace BadApple
+{
+    partial class BadAppleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.aniTimer = new System.Windows.Forms.Timer(this.components);
+            this.enBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.enBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // aniTimer
+            // 
+            this.aniTimer.Tick += new System.EventHandler(this.aniTick);
+            // 
+            // enBox
+            // 
+            this.enBox.BackgroundImage = global::BadApple.Properties.Resources.bad_code_bad;
+            this.enBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.enBox.Location = new System.Drawing.Point(12, 28);
+            this.enBox.Name = "enBox";
+            this.enBox.Size = new System.Drawing.Size(411, 259);
+            this.enBox.TabIndex = 0;
+            this.enBox.TabStop = false;
+            // 
+            // BadAppleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 316);
+            this.Controls.Add(this.enBox);
+            this.Name = "BadAppleForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BadApple";
+            ((System.ComponentModel.ISupportInitialize)(this.enBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer aniTimer;
+        private System.Windows.Forms.PictureBox enBox;
+    }
+}
+
---
+++ BadAppleForm.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BadApple
+{
+    public partial class BadAppleForm : Form
+    {
+        //Pretty bad code ngl
+
+        List<Bitmap> videoFrame = new List<Bitmap>();
+        Button[,] pixels;
+
+        public void loadFrames()
+        {
+            //load the frames from the frames folder (it's already splitted)
+            //TODO: maybe make a splitter into frames
+
+            string frameLocation = "./res/ezgif-frame-";
+            string frameIndex = "";
+
+            for(int i=1; i <= 200; i += 1)
+            {
+                if (i < 10)
+                    frameIndex = frameLocation + "00";
+                else if (i >= 10 && i < 100)
+                    frameIndex = frameLocation + "0";
+                else
+                    frameIndex = frameLocation;
+
+                frameIndex= frameIndex + i.ToString() + ".jpg";
+                videoFrame.Add(new Bitmap(Image.FromFile(frameIndex)));
+            }
+        }
+
+        public void getButtonsReady()
+        {
+            //Generates the button matrix 
+
+            for(int i=0; i < 18; i+=1)
+                for(int j=0; j < 13; j+=1)
+                {
+                    pixels[i, j] = new Button();
+                    pixels[i, j].Width = pixels[i, j].Height = 24;
+                    pixels[i,j].Location = new Point(i*24,j*24);
+
+                    //Adds button to form controls
+                    this.Controls.Add(pixels[i,j]);
+                }
+        }
+
+        
+        public bool bestColor(Bitmap bmp, int x, int y)
+        {
+            //Creates 100 pixel groups and takes the color that it encounter the most
+            int totalSize = 100;
+            int cBlack = 0;
+
+
+            for (int i = x; i < x + 10; i += 1)
+                for (int j = y; j < y + 10; j += 1)
+                    if (bmp.GetPixel(j, i).GetBrightness() < 0.8)
+                        cBlack += 1; //Adds black pixel if the brightness coresponds
+                
+
+            if (totalSize - cBlack > (totalSize / 2))
+                return true; //W
+            return false; //B     
+        }
+        public Bitmap frameKiller(Bitmap bmp)
+        {
+            //Creates the smaller map
+            Bitmap smallerFrame = new Bitmap(20, 15); // /10 map
+
+            int smi = 0, smj = 0;
+
+            
+
+            for (int i = 1; i < bmp.Height-10; i += 10)
+            {
+                smj = 0;
+                for (int j = 1; j < bmp.Width-10; j += 10)
+                {
+
+                    Color clr = bestColor(bmp, i, j) ? Color.White : Color.Black;
+                    smallerFrame.SetPixel(smj, smi, clr);
+                    smj += 1;
+                }
+                smi += 1;
+            }
+
+            return smallerFrame;
+        }
+        
+        public void updateButtons(Bitmap bmp)
+        {
+            for(int i = 0; i < 18; i += 1)
+                for(int j=0; j < 13; j += 1)
+                    pixels[i,j].BackColor = bmp.GetPixel(i, j);
+        }
+
+        
+        PictureBox pb;
+        int frm = 0;
+        private void aniTick(object sender, EventArgs e)
+        {
+            //goes trough each frame and updates buttons
+            if (frm < videoFrame.ToArray().Length-1)
+            { frm += 1; updateButtons(frameKiller(videoFrame[frm])); }
+            else
+            { aniTimer.Stop(); return; }
+            
+        }
+
+        public BadAppleForm()
+        {
+            pixels = new Button[19, 14];
+            InitializeComponent();
+            enBox.Hide();
+            loadFrames();
+            getButtonsReady();
+            aniTimer.Start();
+        }
+
+      
+
+    }
+}
